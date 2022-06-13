bd76af95ceab19af6b8e88e5a0bc2fa7c155a324
akim <akim@pop-os.localdomain>
2022-06-13 13:19:09 +0400

    initialized elm

---
+++ src/Blog.elm
@@ -0,0 +1,9 @@
+module Blog exposing (main)
+import Html exposing (div)
+import Html exposing (text)
+import Html.Attributes exposing (style)
+
+view = 
+    div [style "text-align" "center"] [text "Akim's blog is under development 🚧"]
+
+main = view
