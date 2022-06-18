4a39d195e36f92365292a6c38e2f40be5c0578c7
akim <akim@pop-os.localdomain>
2022-06-18 22:31:46 +0400

    add loading of post list on init

--- src/Blog.elm
+++ src/Blog.elm
@@ -1,18 +1,93 @@
 module Blog exposing (main)
-import Html exposing (div)
+import Html exposing (div, text)
 import Markdown
 import Html.Attributes exposing (class)
+import Browser
+import Dict exposing (update)
+import Http
+import Json.Decode exposing (Decoder, int, list, string, succeed)
+import Json.Decode.Pipeline exposing (optional, required)
+
+
+type alias Post =
+    {
+    title: String
+    ,subtitle: String
+    ,slug: String
+    }
+
+type alias Model =
+    {
+        test: String
+        ,posts: List Post
+        ,loaded: Bool
+    }
+
+
+
+postDecoder: Decoder Post
+postDecoder = 
+    succeed Post
+        |> required "title" string
+        |> optional "subtitle" string ""
+        |> required "slug" string
+
+type Msg = 
+    GotPosts (Result Http.Error (List Post))
 
 content =
     Markdown.toHtml [] """
 # Akim's blog is under development 🚧
 """
 
-view = 
+
+postsPreview: Model -> Html.Html Msg
+postsPreview model = if model.loaded == True
+    then 
+        div [class "content"] (List.map (\post-> div [] [text post.title]) model.posts)
+    else 
+        div [class "content"] [text "Loading Posts..."]
+
+
+view : Model -> Html.Html Msg
+view model = 
     div [class "section"] [
         div [class "container"] [
             div [class "content"] [content]
+            ,postsPreview model
         ]
     ]
 
-main = view
+initialModel : Model
+initialModel = 
+    {
+        test = "123"
+        ,posts = []
+        ,loaded = False
+    }
+
+initialCmd: Cmd Msg
+initialCmd = 
+    Http.get
+    {
+        url = "https://khalitov-blog.azurewebsites.net/api/blog/posts"
+        , expect = Http.expectJson GotPosts (list postDecoder)
+    }
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        GotPosts (Ok posts) ->
+            ( {model | posts = posts, loaded = True}, Cmd.none)
+        GotPosts (Err _) ->
+            ( model, Cmd.none)
+        
+
+main : Program () Model Msg
+main = Browser.element 
+    {
+        init = \_ -> ( initialModel, initialCmd )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+    }
